4effe86dfac681a67b09b0c1b623f6eab6cff35c
Dmytro Anikin <dimianni@users.noreply.github.com>
2022-12-17 12:38:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function calculations() {
+  console.log("Making some calculations");
+  return Math.trunc(Math.random() * 20)
+}
+
 
 function App() {
+
+  // useState returns an array (a tuple) with two predefined elements
+  // useState(calculations()) --> calculations() will get triggered on every state change due to component re-render
+  // BUT if we use a callback function, calculations() will only run on initial mount
+  const [counter, setCounter] = useState(() => {
+    return calculations()
+  })
+
+  const add = () => {
+    setCounter(prevCounter => prevCounter + 1)
+  }
+  const subtract = () => {
+    setCounter(prevCounter => prevCounter - 1)
+  }
+
+
+  const [obj, setObj] = useState({
+    title: "Hello World!",
+    date: Date.now()
+  })
+
+  // If we don't insert previous object (...prevObj) --> date will be gone,
+  // the state will just get changed for the value we passed
+  const changeTitle = () => {
+    setObj(prevObj => {
+      return { ...prevObj, title: "Hello everyone!" }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+
+      {/* Preformatted text */}
+      {/* The text will be displayed exactly as written in the HTML source code. */}
+      <pre>{JSON.stringify(obj, null, 2)}</pre>
+      <button onClick={changeTitle}>Change title</button>
+    </>
   );
 }
 
